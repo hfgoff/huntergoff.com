961346b6b6646e06aec14b43be981953e57b829a
Hunter Goff <hfgoff@wisc.edu>
2025-08-10 20:52:15 -0500

    init bin, Makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+build:
+	go build -o ./bin/huntergoff main.go
--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"net/http"
+)
+
 func main() {
 	// This is a placeholder main function.
 	// You can add your code here to run the application.
+	fs := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fs)
+
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		panic(err)
+	}
 }
