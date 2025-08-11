4583166874c2516534f01f61bc156ecfd98e3a20
Hunter Goff <hfgoff@wisc.edu>
2025-08-10 20:39:06 -0500

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hfgoff/huntergoff
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	// This is a placeholder main function.
+	// You can add your code here to run the application.
+}
